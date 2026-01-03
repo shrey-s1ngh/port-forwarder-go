502bce22d517a2a8b8cb30112eb56e420d9a4ea6
Shrey-mb <shreys0507@gmail.com>
2026-01-03 16:28:32 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module forwarder
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func handle(src net.Conn) {
+	dst, err := net.Dial("tcp", "{unreachable-host}:80")
+	if err != nil {
+		log.Fatalln("Unable to connect to our unreachable host")
+	}
+	defer dst.Close()
+
+	go func() {
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	if _, err := io.Copy(src, dst); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "127.0.0.1:1234")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		go handle(conn)
+	}
+}
